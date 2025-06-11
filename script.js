0ac59c439aa941f34adc976519dc2a66df5ab3ce
Danilo Alves <danilo.tecvip@gmail.com>
2025-06-10 21:40:37 -0300

    Afinando projeto

--- script.js
+++ script.js
@@ -54,3 +54,22 @@ document.addEventListener('DOMContentLoaded', function() {
     
     noSleep.enable();
  }, false);
+
+
+ function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 10 + "s";
+    
+    heart.innerText = '🧡';
+    
+    document.body.appendChild(heart);
+    
+    setTimeout(() => {
+                heart.remove();
+                }, 5000);
+  }
+  
+  setInterval(createHeart, 1000);
